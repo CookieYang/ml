d7e3a2ef9e4b50f4f964660ac17b8102ad2cc4f3
cookieYang <yangshifu12138@icloud.com>
2017-08-22 13:38:53 +0800

    分割数据集函数

--- bmTools.py
+++ bmTools.py
@@ -1,6 +1,11 @@
-from numpy import *
-def splitDataSet(dataSet1,dataSet2,test_proportion = 0.2, circle = 100):
-    m = shape(dataSet1)[0]
-    tm = m - int(test_proportion * m)
-    c = dataSet1[0:tm,:]
-    return dataSet1[0:tm,:],dataSet2[0:tm,:]
+import numpy as np
+import random
+def splitDataSet(dataSet1,dataSet2,test_proportion = 0.2):
+    m = np.shape(dataSet1)[0]
+    total = list(range(m))
+    test = random.sample(range(m),int(test_proportion * m))
+    for i in range(len(test)):
+        total.remove(test[i])
+    return dataSet1[test,:],dataSet2[test,:],dataSet1[total,:],dataSet2[total,:]
+a = np.eye(10)
+c, d, e, f = splitDataSet(a,a)
