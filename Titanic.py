ca47966a6d2ed02bee0f7c2f064468344a544e51
DESKTOP-ABBNTO3\yangtao <1359870821@qq.com>
2017-08-27 22:49:09 +0800

    逻辑回归完成，tensorflow模型的保存和加载

--- Titanic.py
+++ Titanic.py
@@ -2,34 +2,48 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import bmTools
-tData = pd.read_csv(r'/Users/yangtao/Desktop/data/train.csv')
-tData['Sex'] = tData['Sex'].apply(lambda s:1 if s == 'male' else 0)
-tData = tData.fillna(0)
-dataSetX = tData[['Sex','Age','Pclass','SibSp','Parch','Fare']]
-dataSetX = dataSetX.as_matrix()
-tData['Deceased'] = tData['Survived'].apply(lambda s: int(not s))
-dataSetY = tData[['Survived','Deceased']]
-dataSetY = dataSetY.as_matrix()
-x_test, y_test, x_train, y_train = bmTools.splitDataSet(dataSetX,dataSetY)
+# tData = pd.read_csv(r'D:\datas\train.csv')
+# tData['Sex'] = tData['Sex'].apply(lambda s:1 if s == 'male' else 0)
+# tData = tData.fillna(0)
+# dataSetX = tData[['Sex','Age','Pclass','SibSp','Parch','Fare']]
+# dataSetX = dataSetX.as_matrix()
+# tData['Deceased'] = tData['Survived'].apply(lambda s: int(not s))
+# dataSetY = tData[['Survived','Deceased']]
+# dataSetY = dataSetY.as_matrix()
+# x_test, y_test, x_train, y_train = bmTools.splitDataSet(dataSetX,dataSetY)
 x = tf.placeholder(tf.float32, shape=[None,6])
 y = tf.placeholder(tf.float32, shape=[None,2])
 W = tf.Variable(tf.random_normal([6,2]), name = 'weights')
 b = tf.Variable(tf.zeros([2]), name= 'bias')
+saver = tf.train.Saver()
 y_pred = tf.nn.softmax(tf.matmul(x, W) + b)
-cross_entropy = -tf.reduce_sum(y * tf.log(y_pred + 1e-10),reduction_indices=1)
-cost = tf.reduce_mean(cross_entropy)
-train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+# cross_entropy = -tf.reduce_sum(y * tf.log(y_pred + 1e-10),reduction_indices=1)
+# cost = tf.reduce_mean(cross_entropy)
+# train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+# with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+#     tf.global_variables_initializer().run()
+#     for epoch in range(30):
+#         total_loss = 0
+#         for i in range(len(x_train)):
+#             feed = {x:[x_train[i]],y:[y_train[i]]}
+#             _,loss = sess.run([train_op, cost], feed_dict=feed)
+#             total_loss += loss
+#         print('Epoch: %04d,total loss=%.9f' %(epoch + 1,total_loss))
+#     print('Training complete')
+#     save_path = saver.save(sess, 'D:\datas\model.ckpt')
+#     pred = sess.run(y_pred,feed_dict={x:x_test})
+#     correct = np.equal(np.argmax(pred,1),np.argmax(y_test,1))
+#     accuracy = np.mean(correct.astype(np.float32))
+#     print('accuracy is %.9f'% accuracy)
+
+
+
+testData = pd.read_csv(r'D:\datas\test.csv')
+testData = testData.fillna(0)
+testData['Sex'] = testData['Sex'].apply(lambda s: 1 if s == 'male' else 0)
+# saver = tf.train.Saver()
+x_test = testData[['Sex','Age','Pclass','SibSp','Parch','Fare']]
 with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    for epoch in range(10):
-        total_loss = 0
-        for i in range(len(x_train)):
-            feed = {x:[x_train[i]],y:[y_train[i]]}
-            _,loss = sess.run([train_op, cost], feed_dict=feed)
-            total_loss += loss
-        print('Epoch: %04d,total loss=%.9f' %(epoch + 1,total_loss))
-    print('Training complete')
-    pred = sess.run(y_pred,feed_dict={x:x_test})
-    correct = np.equal(np.argmax(pred,1),np.argmax(y_test,1))
-    accuracy = np.mean(correct.astype(np.float32))
-    print('accuracy is %.9f'% accuracy)
+    saver.restore(sess,r'D:\datas\model.ckpt')
+    pred = np.argmax(sess.run(y_pred,feed_dict={x:x_test}))
+    print(pred)
--- logistic.py
+++ logistic.py
@@ -1,16 +1,60 @@
 import bmTools as bt
 import pandas as pd
 import numpy as np
-
+import bmTools as bm
 def sigmod(theta, x):
-    return 1 / 1 + np.exp(- theta.T * x)
+    newMat =  - x * theta
+    return 1 / (1 + np.exp(newMat))
 
-orignalData = pd.read_csv(r'/Users/yangtao/Desktop/data/train.csv')
+def lossFuc(label, x, target):
+    m = np.shape(x)[0]
+    loss = 0
+    for i in range(m):
+        loss += float(label[i][0]) * np.log(float(target[i][0])) + (1 - float(label[i][0])) * np.log( 1 - float(target[i][0]))
+    return - 1 / m * loss
+
+def autoNorm(dataSet):
+    minVals = np.min(dataSet,axis=0)
+    maxVals = np.max(dataSet,axis=0)
+    ranges = maxVals - minVals
+    n = np.zeros(np.shape(dataSet))
+    m = np.shape(dataSet)[0]
+    n = dataSet - np.tile(minVals,(m,1))
+    n = n / np.tile(ranges,(m,1))
+    return n
+
+orignalData = pd.read_csv(r'D:\datas\train.csv')
 orignalData['Sex'] = orignalData['Sex'].apply(lambda s:1 if 'male' == s else 0)
 orignalData = orignalData.fillna(0)
 orignalX = orignalData[['Sex','Age','Pclass','SibSp','Parch','Fare']]
 orignalX = np.matrix(orignalX)
+orignalX = autoNorm(orignalX)
 orignalX = np.column_stack((orignalX,np.ones(np.shape(orignalX)[0])))
-orignalData['Deceased'] = orignalData['Survived'].apply(lambda s: int(not s))
 orignalY = np.matrix(orignalData[['Survived']])
+x_test, y_test, x_train, y_train = bm.splitDataSet(orignalX,orignalY)
 theta = np.ones(np.shape(orignalX)[1])
+theta = np.mat(theta).T
+
+# train
+Hx = sigmod(theta, x_train)
+loss = lossFuc(y_train,x_train,Hx)
+for i in range(300):
+    newH = sigmod(theta, x_train)
+    theta = theta - 0.01 * x_train.T * (newH - y_train)
+    newLoss = lossFuc(y_train, x_train, newH)
+    # if abs(loss - newLoss) < 0.001:
+    #     break
+    # loss = newLoss
+# print(theta)
+result = list(sigmod(theta, x_test))
+result = list(map(lambda x:1 if x >=0.5 else 0,result))
+accuracy = 0
+for i in range(len(result)):
+    if result[i] == y_test[i]:
+        accuracy += 1
+print('accuracy is %.9f'% (accuracy / len(result)))
+
+
+
+
+
